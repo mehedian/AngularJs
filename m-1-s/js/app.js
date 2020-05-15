4d9d053108decb3801ff5d9006b14616a044f04d
mehedian <63111288+mehedian@users.noreply.github.com>
2020-05-15 17:22:29 +0600

    changed

--- m-1-s/js/app.js
+++ m-1-s/js/app.js
@@ -13,15 +13,15 @@
     $scope.check = function () {
       var name = $scope.item.split(",").length;
       if ($scope.item === "") {
-        $scope.update = "enter some input";
+        $scope.update = "Please enter data first";
         $scope.colo = "red";
       }
       else if (name <= 3) {
-        $scope.update = "enjoy";
+        $scope.update = "Enjoy!";
         $scope.colo = "green";
       }
       else {
-        $scope.update = "too much";
+        $scope.update = "Too much!";
         $scope.colo = "yellow";
       }
   
